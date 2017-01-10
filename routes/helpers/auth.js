06883b9b4260d918b62307be508bda79b8c036fe
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-01-10 15:03:23 +0100

    changed wrong url

--- routes/helpers/auth.js
+++ routes/helpers/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   login: function(req, res, next) {
     if (req.session && !req.session.userId) {
       // If there's no SessionID (so no logged in user), rederect
-      res.redirect('/users/login');
+      res.redirect('/login');
     } else {
       // Nothing on the hand, just continue
       next();
