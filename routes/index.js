89db56a86d3c7606352a350c5bc8de2652e1797e
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2017-01-12 09:19:05 +0100

    added cmments to the code

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ const auth = require('./helpers/auth');
 
 /* GET home page. */
 router.get('/', auth.login, function(req, res, next) {
+  // Assign meta
   res.locals.meta = Object.assign({}, res.locals.meta, {
     title: 'MongoDB'
   });
 
-  const url = 'localhost:27017';
-  const db = monk(url);
+  // DB consts
+  const db = monk('localhost:27017');
   const collection = db.get('document');
   const test = db.get('testCollection');
 
@@ -21,23 +22,17 @@ router.get('/', auth.login, function(req, res, next) {
       test.find({})
       .then(testData => {
         console.log('CHECK: testData Found: ');
-        console.log(testData);
         res.render('index', { title: 'MongoDB Test', data: data, test: testData });
       })
-      .catch(() => {
-        console.log('CHECK: No testData Found');
-        res.render('index', { title: 'MongoDB Test', data: data, test: 0 });
-      });
+      .catch(err => { console.log(err); });
     })
-    .catch(err => {
-      console.log(err);
-    })
+    .catch(err => { console.log(err); });
     // .then(() => db.close());
 });
 
 // handle the POST from the add form
 router.post('/', auth.login, (req, res, next) => {
-  // const newThing = req.body;
+  const newThing = req.body;
   console.log('CHECK: got a POST');
 
   // make MongoDB connection
@@ -45,33 +40,25 @@ router.post('/', auth.login, (req, res, next) => {
   const collection = db.get('document');
   const test = db.get('testCollection');
 
-  test.insert({
-    thing: req.body.thing,
-    type: req.body.type
-  })
+  // insert POST into MongoDB
+  test.insert(newThing)
+    // If insert was succesfull
     .then((check) => {
       console.log('CHECK: Posted');
-      console.log(check);
 
-      test.find({})
-      .then(testData => {
-        console.log('CHECK: render after post with test data');
-        console.log(testData);
-        res.render('index', { title: 'MongoDB Test', data: 0, test: testData });
-      })
-      .catch(() => {
-        console.log('CHECK: render after post without test data');
-        res.render('index', { title: 'MongoDB Test', data: 0, test: 0 });
-      });
+      // get the normal collection
+      collection.find({})
+        .then(data => {
 
-      // collection.find({})
-      //   .then(data => {
-      //
-      //   })
-      //   .catch(err => {
-      //     console.log(err);
-      //   })
-      //   .then(() => db.close());
+          // get the test collection
+          test.find({})
+          .then(testData => {
+            console.log('CHECK: render after post with test data');
+            res.render('index', { title: 'MongoDB Test', data: 0, test: testData });
+          })
+          .catch(err => { console.log(err); });
+        })
+        .catch(err => { console.log(err); });
     })
     .catch(err => {
       console.log('CHECK: POST didnt work');
